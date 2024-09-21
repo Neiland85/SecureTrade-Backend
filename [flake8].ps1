4e7de38c5a8bcc634c153096f25913cb7f3334b3
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-09-21 14:43:43 +0200

    Update [flake8].ps1
    "add exclussion .env"

--- [flake8].ps1
+++ [flake8].ps1
@@ -1,7 +1,4 @@
 [flake8]
-# Ignorar reglas de estilo específicas (como líneas largas y espacio antes de :)
 ignore = E501,E203,W391
-# Excluir el directorio venv/ de la revisión de flake8
-exclude = venv/
-# Permitir líneas más largas de lo normal, hasta 120 caracteres
-max-line-length = 120
+exclude = .venv/,*/lib/*,*/site-packages/*
+max-line-length = 100
